8a774dd54679a9bbdea8f69127b26afdecdb74d4
orAlmog <oralmog66@gmail.com>
2022-05-25 19:17:58 +0300

    bdfbdfbdf

--- ChatWebAPI/Hubs/ChatHub.cs
+++ ChatWebAPI/Hubs/ChatHub.cs
@@ -5,13 +5,30 @@ namespace ChatWebAPI.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(Message message, string contactId, string username)
+        static Dictionary<string,string> connections = new Dictionary<string,string>();
+         public void AddConnection(string username)
         {
-            object[] args = new object[3];
-            args[0] = message;
-            args[1] = contactId;
-            args[2] = username;
-            await Clients.All.SendAsync("ReceiveMessage", args);
+            connections[username] = Context.ConnectionId;
+        }
+        public Boolean Authenticate(string username) 
+        { 
+            if(connections.ContainsKey(username) && connections[username] == Context.ConnectionId)
+            {
+                return true;
+            }
+            return false;
+        }
+        public async Task SendMessage(Message message, string username)
+        {
+            //if (Authenticate(username) == false)
+            //{
+            //    return;
+            //}
+            //object[] args = new object[2];
+            //args[0] = message;
+            //args[1] = username;
+            //args[2] = username;
+            await Clients.Client(connections[username]).SendAsync("ReceiveMessage", message);
         }
     }
 }
