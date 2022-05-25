3e3747515a7aa4aa9b850fcd3c8f9033dfc2cc0b
goren <goren.niv@gmail.com>
2022-05-25 12:13:52 +0300

    contactId in signalr

--- ChatWebAPI/Hubs/ChatHub.cs
+++ ChatWebAPI/Hubs/ChatHub.cs
@@ -5,9 +5,13 @@ namespace ChatWebAPI.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(Message message, String userId)
+        public async Task SendMessage(Message message, string contactId, string username)
         {
-            await Clients.User(userId).SendAsync("ReceiveMessage", message);
+            object[] args = new object[3];
+            args[0] = message;
+            args[1] = contactId;
+            args[2] = username;
+            await Clients.All.SendAsync("ReceiveMessage", args);
         }
     }
 }
